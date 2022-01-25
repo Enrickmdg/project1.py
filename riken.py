3369a33ca648689d3d0dd2358675c5e29688e726
Riken <56835872+Enrickmdg@users.noreply.github.com>
2022-01-25 01:44:41 +0100

    Update riken.py

--- riken.py
+++ riken.py
@@ -6,18 +6,23 @@ import os
 sc = nmap.PortScanner()
 
 print("\n****************************************************************************************")
+print("\n****************************************************************************************")
 
 print("""__________          ___________            .__        __    \n
-\______   \___.__.  \_   _____/ ___________|__| ____ |  | __\n
- |    |  _<   |  |   |    __)_ /    \_  __ \  |/ ___\|  |/ /\n
- |    |   \\___  |   |        \   |  \  | \/  \  \___|    < \n
- |______  // ____|  /_______  /___|  /__|  |__|\___  >__|_ \\n
-        \/ \/               \/     \/              \/     \/\n""")
+\______   \___.__.  \_   _____/ ___________|__| ____ |  | __ \n
+ |    |  _<   |  |   |    __)_ /    \_  __ \  |/ ___\|  |/ / \n
+ |    |   \\___  |   |        \   |  \  | \/  \  \___|    <  \n
+ |______  // ____|  /_______  /___|  /__|  |__|\___  >__|_ \ \n
+        \/ \/               \/     \/              \/     \/ \n""")
 
+
+print("\nBienvenue sur mon scirpt de reconnaissance reseau.")
+
+print("\n****************************************************************************************")
 print("\n****************************************************************************************")
 
 def main():
-    n = input("1 - Network scanner\n2 - Vulnerabilities Detection\n3 - Exploit \n\nPlease choose the option you want : ")
+    n = input("1 - Network scanner\n2 - Vulnerabilities Detection\n3 - Exploit \n\nSvp choisissez une option : ")
     if n == '1':
         nmap()
 
@@ -28,17 +33,22 @@ def main():
         os.system('msfconsole')
 
     else :
-        print("There is no valid option with the command you entered. Please choose a number between 1 and 3.")
+        print("Option selectionnee invalide. Svp choisissez une option entre 1 et 3.")
 
 def nmap():
-    print("********************Welcome to the Network Scanner********************")
-    print("**********************************************************************")
-    ip = input("\nPlease enter one IP address:")
+    print("*****************************************************************************")
+    print("********************Vous Avez Selectionner Le Scan Reseau********************")
+    print("*****************************************************************************")
+    ip = input("\nSvp entrez une adresse IP:")
+    sc.scan(ip , '1-1024')
+    print(sc.scaninfo())
+    print(sc[ip]['tcp'].keys())
 
 def vuln():
-    print("********************Welcome to the Vulnerabilites Detection********************")
-    print("*******************************************************************************")
-    ip = input("\nPlease enter the IP address: ")
+    print("*********************************************************************************************")
+    print("********************Vous Avez Selectionner La Detection De Vulnerabilites********************")
+    print("*********************************************************************************************")
+    ip = input("\nSvp entrez une adresse IP: ")
     print(os.system('nmap -sV --script=vulscan.nse' +ip))
 
 if __name__ == '__main__':
